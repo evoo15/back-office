60e7af90326a089543a2ce7486669af626beef02
evoo15 <superyassine15@gmail.com>
2018-06-24 21:36:33 +0100

    add product model

--- src/app/example/formulaire/formulaire.component.ts
+++ src/app/example/formulaire/formulaire.component.ts
@@ -1,6 +1,7 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 import {NgForm} from '@angular/forms' ;
 import {ProduitServiceService} from "../../produit-service.service";
+import {Product} from "../../shared/models/product";
 
 @Component({
   selector: 'app-formulaire',
@@ -11,15 +12,22 @@ import {ProduitServiceService} from "../../produit-service.service";
 @Injectable()
 export class FormulaireComponent implements OnInit {
 
+  product: Product = new Product();
+
   constructor(private produit_service: ProduitServiceService) {
   }
 
   ngOnInit() {
   }
   onSubmit(form: NgForm) {
-   const test = JSON.stringify(form.value);
-    console.log(test) ;
-    this.produit_service.addProduit(form.value)
+    // const test = JSON.stringify(form.value);
+   // console.log(test) ;
+
+    this.product.label = form.value['label'] ;
+    this.product.prix = form.value['prix'] ;
+    this.product.quantite = form.value['quantite'] ;
+
+    this.produit_service.addProduit(JSON.stringify(this.product))
       .subscribe(
       () => alert("produit crée"),
       () => alert("erreur"),
---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,5 @@
+export class Product{
+  label: string ;
+  prix: number;
+  quantite: number ;
+}
